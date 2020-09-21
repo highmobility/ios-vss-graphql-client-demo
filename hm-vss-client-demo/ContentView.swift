26d0f348a633a3dac89a287b7a68420b25daf8c8
doofyus <mikk.ratsep@gmail.com>
2020-09-21 11:03:35 +0200

    Initial Commit

---
+++ hm-vss-client-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hm-vss-client-demo
+//
+//  Created by Mikk Rätsep on 21.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hm-vss-client-demo/hm_vss_client_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  hm_vss_client_demoApp.swift
+//  hm-vss-client-demo
+//
+//  Created by Mikk Rätsep on 21.09.20.
+//
+
+import SwiftUI
+
+@main
+struct hm_vss_client_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
