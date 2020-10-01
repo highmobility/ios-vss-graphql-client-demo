897222ca511a466f395d6ce7a645dda278f2091a
doofyus <mikk.ratsep@gmail.com>
2020-10-01 16:50:22 +0300

    display an error of one occures

--- VSS GraphQL Demo/Views/MainView.swift
+++ VSS GraphQL Demo/Views/MainView.swift
@@ -15,13 +15,21 @@ struct MainView: View {
 
 
     var body: some View {
-        GeometryReader { geometry in
+        let alertBinding = Binding<Bool>(get: { context.error != nil },
+                                         set: { _ in context.error = nil })
+
+        return GeometryReader { geometry in
             ZStack {
                 NavigationView {
                     MainTable()
                         .navigationBarItems(leading: MenuButton(isEditingURL: $isEditingURL),
                                             trailing: SendButton())
                 }
+                .alert(isPresented: alertBinding, content: {
+                    let text = "\(context.error!)"
+
+                    return Alert(title: Text("Error"), message: Text(text))
+                })
 
                 if isEditingURL {
                     VisualEffectView(effect: UIBlurEffect(style: .dark))
